fbb52e1fa8b6677427c85489bd30b24bac7a80b1
ADITHYAA SARAVANAM <adithyaaasaravanam@gmail.com>
2025-02-23 03:50:24 +0530

    (fix): changed db and setup server

--- backend/models/video.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require("mongoose");
-
-const VideoSchema = new mongoose.Schema({
-    title: String,
-    description: String,
-    commits: [{ type: mongoose.Schema.Types.ObjectId, ref: "Commit" }]
-});
-
-module.exports = mongoose.model("Video", VideoSchema);
